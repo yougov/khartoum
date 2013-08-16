65ab8d018542c0eb90f7b8a7e5b4fd089cacabd0
Brent Tubbs <brent.tubbs@gmail.com>
2013-08-16 10:55:54 -0700

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='khartoum',
-    version='0.3.0',
+    version='0.3.1',
     author='Brent Tubbs',
     author_email='brent.tubbs@gmail.com',
     packages=find_packages(),
