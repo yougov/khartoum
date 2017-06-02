9c80c56a96fdc9fd80470f18f6790885c37b7896
Jason R. Coombs <jaraco@jaraco.com>
2017-06-01 20:52:10 -0400

    Align setup script with skeleton

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
+import io
+
 import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+    long_description = readme.read()
+
+name = 'khartoum'
+description = (
+    "A simple app for http serving of static files from MongoDB's GridFS "
+    "filesystem."
+)
 
 
 params = dict(
-    name='khartoum',
+    name=name,
     use_scm_version=True,
     author='YouGov, Plc.',
     author_email='open-source@yougov.com',
+    description=description or name,
+    long_description=long_description,
+    url='https://github.com/yougov/' + name,
     packages=setuptools.find_packages(),
     install_requires=[
         'PyYAML>=3.10',
@@ -13,20 +27,23 @@ params = dict(
         'pymongo>=2.4,<3dev',
         'appsettings==0.3.2',
     ],
+    extras_require={
+    },
     setup_requires=[
         'setuptools_scm>=1.15.0',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
     ],
     entry_points = {
         'console_scripts': [
             'khartoum = khartoum.server:main',
         ],
     },
-    description=(
-        "A simple app for http serving of static files from MongoDB's GridFS "
-        "filesystem."
-    ),
-    long_description=open('README.rst').read(),
-    url='https://github.com/yougov/khartoum',
 )
 
 __name__ == '__main__' and setuptools.setup(**params)
